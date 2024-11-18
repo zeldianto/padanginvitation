c624a38c8136a726705512465c1e522e203a9917
unknown <dev.zeldianto@gmail.com>
2024-11-18 23:47:57 +0700

    feat: adding another catalog items

--- static/assets/site/js/viewer.js
+++ static/assets/site/js/viewer.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+	// const gallery = new Viewer(document.getElementById('imageKu')); // ini untuk menampilkan semua gambar di dalam gallery
     const viewerImages = document.querySelectorAll('.viewer-image');
     viewerImages.forEach(function (image) {
         const viewer = new Viewer(image, {
