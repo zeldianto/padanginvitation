f92b6e2054cb68e4373d7f549ed2c64eb2f27608
unknown <dev.zeldianto@gmail.com>
2025-01-21 00:46:48 +0700

    update tag meta and sitemap

--- src/configs/sitemap.config.js
+++ src/configs/sitemap.config.js
@@ -1,6 +1,7 @@
 module.exports = [
     { loc: 'https://padanginvitation.com/', lastmod: '2024-10-26', changefreq: 'daily', priority: 1.0 },
     { loc: 'https://padanginvitation.com/catalog', lastmod: '2024-10-26', changefreq: 'monthly', priority: 0.9 },
+    { loc: 'https://padanginvitation.com/catalog/printed', lastmod: '2025-01-21', changefreq: 'monthly', priority: 0.9 },
     { loc: 'https://padanginvitation.com/blog', lastmod: '2024-10-26', changefreq: 'monthly', priority: 0.9 },
     { loc: 'https://padanginvitation.com/blog/rekomendasi-lagu-undangan-digital-pernikahan-menyentuh', lastmod: '2024-11-12', changefreq: 'monthly', priority: 0.9 },
     { loc: 'https://padanginvitation.com/blog/warna-undangan-digital-yang-sempurna-beserta-kode-warna', lastmod: '2024-11-13', changefreq: 'monthly', priority: 0.9 },
